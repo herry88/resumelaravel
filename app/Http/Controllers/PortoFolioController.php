c179e46c139de56ca7a1479b599af1552e2aee71
herry88 <herry081288@gmail.com>
2021-03-21 13:36:20 +0700

    resumeweb

---
+++ app/Http/Controllers/PortoFolioController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PortoFolio;
+use Illuminate\Http\Request;
+
+class PortoFolioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('portofolio.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PortoFolio  $portoFolio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PortoFolio $portoFolio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PortoFolio  $portoFolio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PortoFolio $portoFolio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PortoFolio  $portoFolio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PortoFolio $portoFolio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PortoFolio  $portoFolio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PortoFolio $portoFolio)
+    {
+        //
+    }
+}
